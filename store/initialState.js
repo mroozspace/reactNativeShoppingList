2275fb3e7c419bc0d813130b112cb916bf0dfa91
adrian mroz <adrian.mroz@besidethepark.com>
2019-02-11 10:58:28 +0100

    order by date, assets updated

--- store/initialState.js
+++ store/initialState.js
@@ -1,37 +1,6 @@
 const initialState = {
-  shoppingList: [
-    {
-      id: -2,
-      name: 'sniadanie',
-      createdAt: Date.now(),
-      products: [
-        'bagieta czosnkowa',
-        'jajka'
-      ],
-      note: 'biedronka'
-    },
-    {
-      id: -1,
-      name: 'obiad',
-      createdAt: Date.now(),
-      products: [
-        'mleko',
-        'jajka'
-      ],
-      note: 'zabka'
-    }
-  ],
-  archived: [
-    {
-      id: -1,
-      name: 'picie',
-      createdAt: Date.now(),
-      products: [
-        'woda'
-      ],
-      note: 'zabka'
-    }
-  ]
+  shoppingList: [],
+  archived: []
 }
 
 export default initialState
--- store/rootReducer.js
+++ store/rootReducer.js
@@ -3,11 +3,11 @@ import initialState from './initialState';
 const rootReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case 'ADD_SHOPPING_LIST':
-      let id = state.shoppingList[state.shoppingList.length - 1] && state.shoppingList[state.shoppingList.length - 1].id + 1;
-      if ( !id ) {
+    let id=0
+      if (state.shoppingList[state.shoppingList.length - 1]) {
+        id = state.shoppingList[state.shoppingList.length - 1] && state.shoppingList[state.shoppingList.length - 1].id + 1;
+      } else if (state.archived[state.archived.length - 1]) {
         id = state.archived[state.archived.length - 1] && state.archived[state.archived.length - 1].id + 1;
-      } else {
-        id += 1
       }
       return {
         ...state,
@@ -23,22 +23,30 @@ const rootReducer = (state = initialState, { type, payload }) => {
         shoppingList: state.shoppingList
           .filter(item => item.id !== payload.id)
           .concat({ ...payload })
+          .sort( (a,b) => a.createdAt > b.createdAt )
       };
     case 'DELETE_FROM_SHOPPING_LISTS':
       return {
         ...state,
-        shoppingList: state.shoppingList.filter( list => list.id !== payload.id)
+        shoppingList: state.shoppingList
+          .filter( list => list.id !== payload.id)
+          .sort( (a,b) => a.createdAt > b.createdAt )
       };
     case 'ARCHIVE_SHOPPING_LIST':
       return {
         ...state,
-        shoppingList: state.shoppingList.filter(item => item.id !== payload.id),
-        archived: state.archived.concat(payload)
+        shoppingList: state.shoppingList
+          .filter(item => item.id !== payload.id)
+          .sort( (a,b) => a.createdAt > b.createdAt ),
+        archived: state.archived
+          .concat(payload)
+          .sort( (a,b) => a.createdAt > b.createdAt )
       };
     case 'DELETE_FROM_ARCHIVED_LISTS':
       return {
         ...state,
-        archived: state.archived.filter( list => list.id !== payload.id)
+        archived: state.archived
+          .filter( list => list.id !== payload.id)
       };
     default:
       return state;
